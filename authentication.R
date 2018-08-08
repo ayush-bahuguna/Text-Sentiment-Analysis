ad0413faec0073027156a49456e06b45c26dbad4
ayush-bahuguna <ayushbahuguna7@gmail.com>
2018-08-09 00:46:21 +0530

    Update authentication.R

--- authentication.R
+++ authentication.R
@@ -5,17 +5,13 @@
 library(ROAuth)
 library(twitteR)
 
-consumer_key <- "ENZNdW2IRfoUI5OvKAAwGswxp"
-#Insert consumer key
+consumer_key <- #Insert consumer key
   
-consumer_secret <- "OyAC4eFwatmaHvhliFMchtpqndC4pikfnADXuBfZ7jyZEIYs89"
-#Insert consumer secret
+consumer_secret <- #Insert consumer secret
 
-access_token <- "1019675583800922112-SjTTkHYKtpCowXeiXkvhu3NYsd06Na"
-#Insert access token
+access_token <- #Insert access token
   
-access_secret <- "qT8MbksUeOnczKpaKD1Up2MsaFV0qDb7Mywj0pVhIdi4d"
-#Insert access secret
+access_secret <- #Insert access secret
  
 download.file(url="http://curl.haxx.se/ca/cacert.pem", destfile="cacert.pem")
 #downloads the certificate
